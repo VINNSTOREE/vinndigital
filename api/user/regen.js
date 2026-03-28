08be8a067c04dd4bd1cfe14e29684030da90feb3
VINNSTOREE <vingg1633@gmail.com>
2026-03-29 01:50:28 +0700

    Update regen.js

--- api/user/regen.js
+++ api/user/regen.js
@@ -1,17 +1,29 @@
-import crypto from "crypto";
-import { getDB } from "../../lib/db.js";
-import { verifyToken } from "../../lib/auth.js";
+const { getDB } = require("../../lib/db");
+const jwt = require("jsonwebtoken");
+const { CONFIG } = require("../../lib/config");
 
-export default async function handler(req, res) {
-  const userData = verifyToken(req);
-  const db = await getDB();
+function generateApiKey(){
+  return "vp_" + Math.random().toString(36).substring(2,10) + Date.now();
+}
 
-  const newKey = "vk_" + crypto.randomBytes(16).toString("hex");
+module.exports = async function handler(req, res){
+  try{
+    const token = req.headers.authorization;
 
-  await db.collection("users").updateOne(
-    { _id: userData.id },
-    { $set: { apiKey: newKey } }
-  );
+    const decode = jwt.verify(token, CONFIG.JWT_SECRET);
 
-  res.json({ success: true });
+    const db = await getDB();
+
+    const newKey = generateApiKey();
+
+    await db.collection("users").updateOne(
+      { _id: require("mongodb").ObjectId.createFromHexString(decode.id) },
+      { $set: { apiKey: newKey } }
+    );
+
+    res.json({ success:true, apiKey:newKey });
+
+  }catch(err){
+    res.json({ success:false });
+  }
 }
