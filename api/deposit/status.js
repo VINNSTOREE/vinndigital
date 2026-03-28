0002da649a2726ef297295f1792fca9b321473b9
VINNSTOREE <vingg1633@gmail.com>
2026-03-29 00:46:41 +0700

    Update status.js

--- api/deposit/status.js
+++ api/deposit/status.js
@@ -1,7 +1,20 @@
 import { CONFIG } from "../../lib/config.js";
 
-const r = await fetch(`https://ramashop.my.id/api/public/deposit/status/${id}`, {
-  headers: {
-    "X-API-Key": CONFIG.API_KEY
+export default async function handler(req, res) {
+  try {
+    const { id } = req.query;
+
+    const r = await fetch(`https://ramashop.my.id/api/public/deposit/status/${id}`, {
+      headers: {
+        "X-API-Key": CONFIG.API_KEY
+      }
+    });
+
+    const data = await r.json();
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-});
+}
