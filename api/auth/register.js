dd718655e3734a13c99cbbf5f93779146eb026b3
VINNSTOREE <vingg1633@gmail.com>
2026-03-29 01:03:50 +0700

    Update register.js

--- api/auth/register.js
+++ api/auth/register.js
@@ -1,42 +1,13 @@
-import { getDB } from "../../lib/db.js";
-import bcrypt from "bcryptjs";
+import crypto from "crypto";
 
-export default async function handler(req, res) {
-  try {
-    if (req.method !== "POST") {
-      return res.status(405).json({ success: false });
-    }
+const apiKey = "vk_" + crypto.randomBytes(16).toString("hex");
 
-    const { email, password } = req.body;
-
-    const db = await getDB();
-
-    const exist = await db.collection("users").findOne({ email });
-    if (exist) {
-      return res.status(400).json({
-        success: false,
-        message: "Email sudah terdaftar"
-      });
-    }
-
-    const hash = await bcrypt.hash(password, 10);
-
-    await db.collection("users").insertOne({
-      email,
-      password: hash,
-      saldo: 0,
-      createdAt: new Date()
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "Register berhasil"
-    });
-
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      error: err.message
-    });
-  }
-}
+await db.collection("users").insertOne({
+  email,
+  password: hash,
+  saldo: 0,
+  apiKey,
+  requestCount: 0,
+  lastUsed: null,
+  createdAt: new Date()
+});
