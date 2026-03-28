5773a97331d309051e2fb32dd21ef7e549bad699
VINNSTOREE <vingg1633@gmail.com>
2026-03-29 00:45:37 +0700

    Update register.js

--- api/auth/register.js
+++ api/auth/register.js
@@ -9,13 +9,6 @@ export default async function handler(req, res) {
 
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({
-        success: false,
-        message: "Email & password wajib"
-      });
-    }
-
     const db = await getDB();
 
     const exist = await db.collection("users").findOne({ email });
@@ -35,15 +28,15 @@ export default async function handler(req, res) {
       createdAt: new Date()
     });
 
-    return res.status(200).json({
+    res.status(200).json({
       success: true,
       message: "Register berhasil"
     });
 
   } catch (err) {
-    return res.status(500).json({
+    res.status(500).json({
       success: false,
       error: err.message
     });
   }
-      }
+}
