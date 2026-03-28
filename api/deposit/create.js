53a5c03bae82cf256b63cc289608d5812ec7e683
VINNSTOREE <vingg1633@gmail.com>
2026-03-29 00:46:27 +0700

    Update create.js

--- api/deposit/create.js
+++ api/deposit/create.js
@@ -1,10 +1,23 @@
 import { CONFIG } from "../../lib/config.js";
 
-const r = await fetch("https://ramashop.my.id/api/public/deposit/create", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "X-API-Key": CONFIG.API_KEY
-  },
-  body: JSON.stringify({ amount })
-});
+export default async function handler(req, res) {
+  try {
+    const { amount } = req.body;
+
+    const r = await fetch("https://ramashop.my.id/api/public/deposit/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-Key": CONFIG.API_KEY
+      },
+      body: JSON.stringify({ amount })
+    });
+
+    const data = await r.json();
+
+    res.status(200).json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
